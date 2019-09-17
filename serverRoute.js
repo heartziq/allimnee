61b10c19b4f89747eb4500604486a08980d7a1be
jane <rowj47250@gmail.com>
2019-09-17 12:21:05 +0800

    db async await issue solved

--- serverRoute.js
+++ serverRoute.js
@@ -2,10 +2,49 @@ import { serverRender } from "./serverRender";
 import fetch from "isomorphic-fetch";
 import { MongoClient } from "mongodb";
 
+// const myMongo = new MongoClient("mongodb://localhost:27017/test", {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true
+// });
+
+// const getAllTutor = async reply => {
+//   await myMongo.connect(async (err, result) => {
+//     await result
+//       .db("test")
+//       .collection("tutor")
+//       .find({})
+//       .toArray()
+//       .then(res => {
+//         reply = reply.response(res)
+//         // return res;
+//         // result.close();
+//         // console.log(res)
+//         // return 'not read';
+//       });
+//   });
+//   return reply.code(200);
+// };
+
+// (async () => {
+//   const result = await getAllTutor();
+//   console.log(`result: ${result}`);
+// })();
+
 module.exports = {
   name: "myPlugin",
   version: "1.0.0",
   register: async function(server, options) {
+    // initiate Mongo Connection
+    const myMongo = new MongoClient("mongodb://localhost:27017/test", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+
+    let conn;
+    await myMongo.connect(async (err, result) => {
+      conn = await result.db("test").collection("tutor");
+    });
+
     // Main Page
     server.route({
       method: "GET",
@@ -52,37 +91,29 @@ module.exports = {
     server.route({
       method: "GET",
       path: "/api/tutor",
-      handler: function(request, reply) {
-        console.log("this runs!");
+      handler: async function(request, reply) {
+        console.log("calling /api/tutor ...");
         // api tutor list
         // initiate Mongo Connection
-        const myMongo = new MongoClient("mongodb://localhost:27017/test", {
-          useUnifiedTopology: true,
-          useNewUrlParser: true
-        });
-        let dataObj;
-        return myMongo.connect(async (err, result) => {
-          dataObj = await result
-            .db("test")
-            .collection("tutor")
-            .find({})
-            .toArray()
-            .then(res => {
-              // return reply.response(res).code(200)
-              return res;
-            });
-
-          return reply.response(dataObj).code(200);
-        });
-      }
-    });
-
-    // test
-    server.route({
-      method: "GET",
-      path: "/test",
-      handler: function(request, reply) {
-        return reply.response({ data: "helloworld" }).code(200);
+        // const myMongo = new MongoClient("mongodb://localhost:27017/test", {
+        //   useUnifiedTopology: true,
+        //   useNewUrlParser: true
+        // });
+        // let dataObj;
+        // const dataObj = await ....
+        // await myMongo.connect(async (err, result) => {
+        //   dataObj = await result
+        //     .db("test")
+        //     .collection("tutor")
+        //     .find({})
+        //     .toArray()
+        //     .then(res => {
+        //       // return reply.response(res).code(200)
+        //       return res;
+        //     });
+        // });
+        const qResult = await conn.find({}).toArray();
+        return reply.response(qResult).code(200);
       }
     });
   }
