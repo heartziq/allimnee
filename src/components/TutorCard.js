58b354a221eda20bc304d354703670ec8645c1a7
jane <rowj47250@gmail.com>
2019-10-15 11:42:28 +0800

    deleted unwanted lines

--- src/components/TutorCard.js
+++ src/components/TutorCard.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import fetch from "isomorphic-fetch";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
 
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
